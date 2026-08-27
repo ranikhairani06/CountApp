0c7925f034f56d701c1a157ed89cbccdcd37992f
nikmahputrikhairani <nikmahrani006@gmail.com>
2026-08-27 09:02:08 +0700

    initial commit change to linear layout

--- app/src/main/java/com/example/countapp/MainActivity.kt
+++ app/src/main/java/com/example/countapp/MainActivity.kt
@@ -5,16 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.countapp.databinding.ActivityMainBinding
 
+//variable untuk binding
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //inisiasi binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //set content dari activity
+        setContentView(binding.root)
+
+        //akses view element
+        with(receiver = binding) {
+
         }
     }
 }
